7153c352730a17b6495e67bc9ef018918bac36d3
gabrielcruz07 <cruzsouza2002@gmail.com>
2024-10-01 17:43:59 -0300

    Subindo javascript finalizado

--- site/home/script.js
+++ site/home/script.js
@@ -12,31 +12,24 @@ function continuarAnimacao() {
 noticiasDiv.addEventListener("mouseover", pausarAnimacao);
 noticiasDiv.addEventListener("mouseout", continuarAnimacao);
 
-// Data final da contagem regressiva
 var countDownDate = new Date("Oct 31, 2024 00:00:00").getTime();
 
-// Atualiza a contagem regressiva a cada 1 segundo
 var x = setInterval(function() {
 
-  // Data e hora atuais
   var now = new Date().getTime();
 
-  // Diferença entre a data final e a atual
   var distance = countDownDate - now;
 
-  // Cálculo de dias, horas, minutos e segundos
   var days = Math.floor(distance / (1000 * 60 * 60 * 24));
   var hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
   var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
   var seconds = Math.floor((distance % (1000 * 60)) / 1000);
 
-  // Exibe os resultados no elemento HTML com id="countdown"
   document.getElementById("days").innerHTML = days;
   document.getElementById("hours").innerHTML = hours;
   document.getElementById("minutes").innerHTML = minutes;
   document.getElementById("seconds").innerHTML = seconds;
 
-  // Quando a contagem regressiva termina
   if (distance < 0) {
     clearInterval(x);
     document.getElementById("countdown").innerHTML = "TEMPO ESGOTADO!";
